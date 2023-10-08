ecc9ba3e8374fd3ad0911cedd058bbee42c3faa8
Joanne Ndirangu <joannendirangu15@gmail.com>
2023-10-08 03:00:33 +0300

    Completed assignment

--- server/models.py
+++ server/models.py
@@ -11,13 +11,22 @@ class Zookeeper(db.Model):
     __tablename__ = 'zookeepers'
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    birthday = db.Column(db.String)
+    animals = db.Column(db.String)
 
 class Enclosure(db.Model):
     __tablename__ = 'enclosures'
 
     id = db.Column(db.Integer, primary_key=True)
+    animals = db.Column(db.String)
 
 class Animal(db.Model):
     __tablename__ = 'animals'
+
+    name = db.Column(db.String)
+    species = db.Column(db.String)
+    zookeeper = db.Column(db.String)
+    enclosure = db.Column(db.String)
 
     id = db.Column(db.Integer, primary_key=True)
